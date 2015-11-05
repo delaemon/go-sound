bdaabeff95479f3414ae5c400a5dab91c457bf3e
delaemon <takeshi.music.5@gmail.com>
2015-11-06 00:15:09 +0900

    Remove debug

--- example/play.go
+++ example/play.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/delaemon/go-sound"
-	"fmt"
 )
 
 var filename = flag.String("file", "bazinga.wav", "play file")
@@ -13,7 +12,6 @@ var times = flag.Int("times", 1, "play times")
 
 func main() {
 	flag.Parse()
-	fmt.Println(*times)
 	for i := 0; i < *times; i++ {
 		sound.Play(*filename)
 		time.Sleep(1 * time.Second)
